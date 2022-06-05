926b8eee1f55e0efa2dc1094dd7f1658b268b785
Robert Norwood <you@example.com>
2022-06-04 17:17:22 -0700

    Created the Tortoise class and was able to get the turtle to show up on the screen.

---
+++ tortoise.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+class Tortoise(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("White")
+        self.penup()
+
+
+

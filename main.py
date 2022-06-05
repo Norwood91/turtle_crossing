a3094781eac10055b684bb3b7be9fb319b116e26
Robert Norwood <you@example.com>
2022-06-04 18:20:25 -0700

    Created the scoreboard and cars files with some basic starter code.

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import time
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.title("Crossing by Robert L. Norwood")
 screen.tracer(0)
 
 tortoise = Tortoise()
 
 screen.listen()
-
+screen.onkey(tortoise.move, "Up")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+
 
 
 screen.exitonclick()
--- tortoise.py
+++ tortoise.py
@@ -1,11 +1,23 @@
 from turtle import Turtle
 
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE = 280
+UP = 90
+
 class Tortoise(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
         self.color("White")
         self.penup()
+        self.head = self
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        self.head.setheading(UP)
+        self.head.forward(MOVE_DISTANCE)
+
 
 
 

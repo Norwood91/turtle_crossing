c6f99201bd679a64552a4bab3472fb305abe01f0
Robert Norwood <you@example.com>
2022-06-04 17:15:48 -0700

    Created the Tortoise class and was able to get the turtle to show up on the screen.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from turtle import Screen
-
+from tortoise import Tortoise
+import time
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.tracer(0)
+
+tortoise = Tortoise()
+
+screen.listen()
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
